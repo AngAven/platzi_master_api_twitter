9efd407260285ccd8eb9452a9b26e3fd5164e371
Angel <angel.aven.c@gmail.com>
2021-11-20 17:50:48 -0600

    Simple filtered stream

--- src/filtered_stream.js
+++ src/filtered_stream.js
@@ -1,15 +1,15 @@
 // Open a realtime stream of Tweets, filtered according to rules
 // https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/quick-start
 
-const needle = require('needle');
+const needle = require('needle')
 require('dotenv').config()
 
 // The code below sets the bearer token from your environment variables
 // To set environment variables on macOS or Linux, run the export command below from the terminal:
 // export BEARER_TOKEN='YOUR-TOKEN'
-const token = process.env.BEARER_TOKEN;
-const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
-const streamURL = 'https://api.twitter.com/2/tweets/search/stream';
+const token = process.env.BEARER_TOKEN
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream'
 
 // this sets up two rules - the value is the search terms to match on, and the tag is an identifier that
 // will be applied to the Tweets return to show which rule they matched
@@ -17,100 +17,97 @@ const streamURL = 'https://api.twitter.com/2/tweets/search/stream';
 // each rule can be up to 512 characters long
 
 // Edit rules as desired below
-const rules = [{
-  'value': 'dog has:images -is:retweet',
-  'tag': 'dog pictures'
-},
+const rules = [
   {
-    'value': 'cat has:images -grumpy',
-    'tag': 'cat pictures'
-  },
-];
+  'value': 'btc has:images -is:retweet',
+  'tag': 'bitcoin bitso'
+  }
+]
 
-async function getAllRules() {
+async function getAllRules(){
 
   const response = await needle('get', rulesURL, {
     headers: {
-      "authorization": `Bearer ${token}`
+      'authorization': `Bearer ${token}`
     }
   })
 
   if (response.statusCode !== 200) {
-    console.log("Error:", response.statusMessage, response.statusCode)
-    throw new Error(response.body);
+    console.log('Error:', response.statusMessage, response.statusCode)
+    throw new Error(response.body)
   }
 
-  return (response.body);
+  return (response.body)
 }
 
-async function deleteAllRules(rules) {
+async function deleteAllRules(rules){
 
   if (!Array.isArray(rules.data)) {
-    return null;
+    return null
   }
 
-  const ids = rules.data.map(rule => rule.id);
+  const ids = rules.data.map(rule => rule.id)
 
   const data = {
-    "delete": {
-      "ids": ids
+    'delete': {
+      'ids': ids
     }
   }
 
   const response = await needle('post', rulesURL, data, {
     headers: {
-      "content-type": "application/json",
-      "authorization": `Bearer ${token}`
+      'content-type': 'application/json',
+      'authorization': `Bearer ${token}`
     }
   })
 
   if (response.statusCode !== 200) {
-    throw new Error(response.body);
+    throw new Error(response.body)
   }
 
-  return (response.body);
+  return (response.body)
 
 }
 
-async function setRules() {
+async function setRules(){
 
   const data = {
-    "add": rules
+    'add': rules
   }
 
   const response = await needle('post', rulesURL, data, {
     headers: {
-      "content-type": "application/json",
-      "authorization": `Bearer ${token}`
+      'content-type': 'application/json',
+      'authorization': `Bearer ${token}`
     }
   })
 
   if (response.statusCode !== 201) {
-    throw new Error(response.body);
+    throw new Error(response.body)
   }
 
-  return (response.body);
+  return (response.body)
 
 }
 
-function streamConnect(retryAttempt) {
+function streamConnect(retryAttempt){
 
   const stream = needle.get(streamURL, {
     headers: {
-      "User-Agent": "v2FilterStreamJS",
-      "Authorization": `Bearer ${token}`
+      'User-Agent': 'v2FilterStreamJS',
+      'Authorization': `Bearer ${token}`
     },
     timeout: 20000
-  });
+  })
 
   stream.on('data', data => {
     try {
-      const json = JSON.parse(data);
-      console.log(json);
+      const json = JSON.parse(data)
+      console.log(json)
       // A successful connection resets retry count.
-      retryAttempt = 0;
+      retryAttempt = 0
     } catch (e) {
-      if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
+      if (data.detail === 'This stream is currently at the maximum allowed connection limit.') {
         console.log(data.detail)
         process.exit(1)
       } else {
@@ -119,42 +116,42 @@ function streamConnect(retryAttempt) {
     }
   }).on('err', error => {
     if (error.code !== 'ECONNRESET') {
-      console.log(error.code);
-      process.exit(1);
+      console.log(error.code)
+      process.exit(1)
     } else {
       // This reconnection logic will attempt to reconnect when a disconnection is detected.
       // To avoid rate limits, this logic implements exponential backoff, so the wait time
       // will increase if the client cannot reconnect to the stream.
       setTimeout(() => {
-        console.warn("A connection error occurred. Reconnecting...")
-        streamConnect(++retryAttempt);
+        console.warn('A connection error occurred. Reconnecting...')
+        streamConnect(++retryAttempt)
       }, 2 ** retryAttempt)
     }
-  });
+  })
 
-  return stream;
+  return stream
 
 }
 
 
 (async () => {
-  let currentRules;
+  let currentRules
 
   try {
     // Gets the complete list of rules currently applied to the stream
-    currentRules = await getAllRules();
+    currentRules = await getAllRules()
 
     // Delete all rules. Comment the line below if you want to keep your existing rules.
-    await deleteAllRules(currentRules);
+    await deleteAllRules(currentRules)
 
     // Add rules to the stream. Comment the line below if you don't want to add new rules.
-    await setRules();
+    await setRules()
 
   } catch (e) {
-    console.error(e);
-    process.exit(1);
+    console.error(e)
+    process.exit(1)
   }
 
   // Listen to the stream.
-  streamConnect(0);
-})();
+  streamConnect(0)
+})()
